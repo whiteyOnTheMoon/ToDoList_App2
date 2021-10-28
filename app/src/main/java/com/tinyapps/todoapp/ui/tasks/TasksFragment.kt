5d9e21079c2f08b47e575f36fc2faa36213a0793
Alfred <hi@alfred-paier.com>
2021-10-28 16:32:37 +0200

    Part 6 of Tutorial finished

--- app/src/main/java/com/tinyapps/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/tinyapps/todoapp/ui/tasks/TasksFragment.kt
@@ -1,16 +1,21 @@
 package com.tinyapps.todoapp.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tinyapps.todoapp.R
 import com.tinyapps.todoapp.databinding.FragmentTasksBinding
+import com.tinyapps.todoapp.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TasksFragment : Fragment(R.layout.fragment_tasks){
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
 
@@ -29,8 +34,40 @@ class TasksFragment : Fragment(R.layout.fragment_tasks){
             }
         }
 
-        viewModel.tasks.observe(viewLifecycleOwner){
+        viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_task, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/tinyapps/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/tinyapps/todoapp/ui/tasks/TasksViewModel.kt
@@ -4,10 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.tinyapps.todoapp.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
 
-    val tasks = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+    private val taskFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+    val tasks = taskFlow.asLiveData()
 }
